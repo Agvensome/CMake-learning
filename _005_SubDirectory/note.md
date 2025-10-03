3c05620ea33a946f8e13312b0e177f1aae8509fe
Agvensome <651752508@qq.com>
2025-10-03 13:53:34 +0800

    005 SubDirectory

---
+++ _005_SubDirectory/note.md
@@ -0,0 +1,4 @@
+
+#### 005 append subdirectory
+    ADD_SUBDIRECTORY(src_dir [bin_dir]
+                    [EXCLUDE_FROM_ALL])
---
+++ _005_SubDirectory/src/main.cpp
@@ -0,0 +1,7 @@
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::cout << "This is a subdirectory project!" << std::endl;
+    return EXIT_SUCCESS;
+}
