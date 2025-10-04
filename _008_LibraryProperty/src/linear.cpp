460fda9c2de0fdcc594589b7d7c18f4bc0968b65
Agvensome <651752508@qq.com>
2025-10-04 16:45:16 +0800

    008 LibraryProperty

---
+++ _008_LibraryProperty/src/linear.cpp
@@ -0,0 +1,7 @@
+
+#include <iostream>
+#include "linear.h"
+
+void linear_fit() {
+    std::cout << "linear fit called!" << std::endl;
+}
---
+++ _008_LibraryProperty/src/linear.h
@@ -0,0 +1,7 @@
+
+#ifndef __LINEAR_H__
+#define __LINEAR_H__
+
+void linear_fit();
+
+#endif
